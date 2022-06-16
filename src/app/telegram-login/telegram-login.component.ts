74d4bd1bae93d7e1e8eda97d63c575bb95bd7e7a
noufal <noufalnasar11@gmail.com>
2022-06-17 02:49:22 +0530

    socialCash web apllication uidesign

---
+++ src/app/telegram-login/telegram-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-telegram-login',
+  templateUrl: './telegram-login.component.html',
+  styleUrls: ['./telegram-login.component.css']
+})
+export class TelegramLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
